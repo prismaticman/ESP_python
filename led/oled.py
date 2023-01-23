79f77351272cae50d6ac58cc19332ffecbe589d4
27794 <2779435956@qq.com>
2023-01-23 16:25:36 +0800

    doc[doc]:修改oled font字典

--- led/oled.py
+++ led/oled.py
@@ -9,12 +9,11 @@ from machine import Pin, I2C
 import ssd1306
 import framebuf
 
-i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 display.fill(0)
 display.text('MicroPython', 40, 0, 1)
 display.text('SSD1306', 40, 12, 1)
 display.text('OLED 128x64', 40, 24, 1)
-display.show()
 display.show()
